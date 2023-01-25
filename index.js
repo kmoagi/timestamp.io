84d6a9b64242e3a402b9db8895a6c4fe7f9da902
Kabelo <33934323+kmoagi@users.noreply.github.com>
2023-01-25 17:43:47 +0200

    Updated index.js file

--- index.js
+++ index.js
@@ -8,25 +8,51 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: "Helllo API" })
 });
+
+
+app.get("/api", (req, res) => {
+  res.json({ unix: new Date().getTime(), utc: new Date().toUTCString() })
+});
+
+
+app.get("/api/:timestamp", (req, res) => {
+  const timestamp = req.params.timestamp;
+  if (!isNaN(Number(timestamp)) && timestamp.length === 13)
+    return res.json({
+      unix: timestamp,
+      utc: new Date(Number(timestamp)).toUTCString(timestamp)
+    });
+
+  if (new Date().toUTCString() !== "Invalid Date") {
+
+    return res.json({
+      unix: new Date(timestamp).getTime(),
+      utc: new Date(timestamp).toUTCString(),
+    })
+  }
+
+  res.json({ error: "Invalid Date" });
+})
+
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
